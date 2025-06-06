856adf917db4d074b41b5121fb95ed3e2c01ae7d
Jim G <gazaway85@gmail.com>
2025-06-06 12:56:03 -0600

    reduce files.

--- HealthChecker/GlobalUsings.cs
+++ HealthChecker/GlobalUsings.cs
@@ -1,6 +1,4 @@
 global using Fonts;
-global using HealthChecker.Data;
 global using HealthChecker.PageModels;
 global using HealthChecker.Pages;
 global using HealthChecker.Services;
-global using HealthChecker.Utilities;
--- HealthChecker/Models/IconData.cs
+++
@@ -1,8 +0,0 @@
-namespace HealthChecker.Models
-{
-    public class IconData
-    {
-        public string? Icon { get; set; }
-        public string? Description { get; set; }
-    }
-}
